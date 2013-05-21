b00b45a9ce6e2336fedcee3562f14faa59238f2e
sararob <sararob@brandeis.edu>
2013-05-21 11:39:51 -0700

    Update web.js

--- app/js/web.js
+++ app/js/web.js
@@ -3,7 +3,7 @@ var static = require('node-static');
 //
 // Create a node-static server instance to serve the './app' folder
 //
-var file = new(static.Server)('./app');
+var file = new(static.Server)('./app/js');
 var port = process.env.PORT || 5000;
 require('http').createServer(function (request, response) {
     request.addListener('end', function () {
